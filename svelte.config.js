3d39f620a03399d13661c4e2cdcfcf2ebeb72c5f
Pedro Thiago <pedrothiagojosedasilva@gmail.com>
2024-04-18 10:56:51 -0300

    workflow run

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-const dev = process.argv.includes('dev');
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -22,6 +22,6 @@ const config = {
 			base: dev ? '' : process.env.BASE_PATH,
 		}
 	}
-};
+}
 
-export default config;
+export default config
